bda55c0c2d0f41b0a77a7c38528f99d886c6a135
holuuduc0104 <holuuduc0104@gmail.com>
2025-12-11 07:10:41 +0700

    save dish to favorite list

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -3,16 +3,19 @@ import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 import { IngredientsProvider } from '@/context/IngredientsContext';
+import { AuthProvider } from '../context/AuthContext'; // Điều chỉnh đường dẫn nếu cần
 
 export default function RootLayout() {
   useFrameworkReady();
 
   return (
-    <IngredientsProvider>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </IngredientsProvider>
+    <AuthProvider> {/* Thêm AuthProvider ở đây */}
+      <IngredientsProvider>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="+not-found" />
+        </Stack>
+        <StatusBar style="auto" />
+      </IngredientsProvider>
+    </AuthProvider>
   );
 }
